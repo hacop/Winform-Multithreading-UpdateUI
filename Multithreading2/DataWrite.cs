94664b503467c10c4b87d85541ad043d39f20cd4
Hauss <sihaojj@163.com>
2019-03-17 22:29:31 +0800

    Intial

---
+++ Multithreading2/DataWrite.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading2
+{
+    public class DataWrite
+    {
+        public delegate void UpdateUI(int step, int taskid);//声明一个更新主线程的委托
+        public UpdateUI UpdateUIDelegate;
+
+        public delegate void AccomplishTask();//声明一个在完成任务时通知主线程的委托
+        public AccomplishTask TaskCallBack;
+
+        public void Write(object lineCount)
+        {
+            int[] temp = (int[])lineCount;
+            StreamWriter writeIO = new StreamWriter("text" + temp [1]+ ".txt", false, Encoding.GetEncoding("gb2312"));
+            string head = "编号,省,市";
+            writeIO.Write(head);
+
+            for (int i = 0; i < temp[0]; i++)
+            {
+                writeIO.WriteLine(i.ToString() + ",湖南,衡阳");
+                //写入一条数据，调用更新主线程ui状态的委托
+                UpdateUIDelegate(1, temp[1]);
+            }
+            //任务完成时通知主线程作出相应的处理
+            TaskCallBack();
+            writeIO.Close();
+        }
+    }
+}
---
+++ Multithreading2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multithreading2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        delegate void AsynUpdateUI(int step);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] taskCount = { 5, 0 }; //任务量为10000
+            this.pgbWrite.Maximum = taskCount[0];
+            this.pgbWrite.Value = 0;
+
+            int[] taskCount1 = { 10000, 1 }; //任务量为10000
+            this.progressBar1.Maximum = taskCount1[0];
+            this.progressBar1.Value = 0;
+
+
+            DataWrite dataWrite = new DataWrite();//实例化一个写入数据的类
+            dataWrite.UpdateUIDelegate += UpdataUIStatus;//绑定更新任务状态的委托
+            dataWrite.TaskCallBack += Accomplish;//绑定完成任务要调用的委托
+
+            Thread thread = new Thread(new ParameterizedThreadStart(dataWrite.Write))
+            {
+                IsBackground = true
+            };
+            thread.Start(taskCount);
+
+            Thread thread1 = new Thread(new ParameterizedThreadStart(dataWrite.Write))
+            {
+                IsBackground = true
+            };
+            thread1.Start(taskCount1);
+        }
+        //更新UI
+        private void UpdataUIStatus(int step, int taskid)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new AsynUpdateUI(delegate (int s)
+                {
+                    if (taskid == 0)
+                    {
+                        this.pgbWrite.Value += s;
+                        this.lblWriteStatus.Text = this.pgbWrite.Value.ToString() + "/" + this.pgbWrite.Maximum.ToString();
+                    }
+                    else
+                    {
+                        this.progressBar1.Value += s;
+                        this.label1.Text = this.progressBar1.Value.ToString() + "/" + this.progressBar1.Maximum.ToString();
+                    }
+                }), step);
+            }
+            else
+            {
+                if (taskid == 0)
+                {
+                    this.pgbWrite.Value += step;
+                    this.lblWriteStatus.Text = this.pgbWrite.Value.ToString() + "/" + this.pgbWrite.Maximum.ToString();
+                }
+                else
+                {
+                    this.progressBar1.Value += step;
+                    this.label1.Text = this.progressBar1.Value.ToString() + "/" + this.progressBar1.Maximum.ToString();
+                }
+            }
+        }
+
+        //完成任务时需要调用
+        private void Accomplish()
+        {
+            //还可以进行其他的一些完任务完成之后的逻辑处理
+            MessageBox.Show("任务完成");
+        }
+    }
+}
